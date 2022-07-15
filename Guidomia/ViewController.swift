2f088c63d4c7977e4262ff3942643f6a7d47fdc9
Roman Kavisnkyi <roman_kavinskyi@pecodesoftware.com>
2022-07-15 17:27:04 +0700

    Initial Commit

---
+++ Guidomia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia
+//
+//  Created by Roman Kavinskyi on 7/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
